c7868eab1f3fb5da1764aa8eb4099f4dcbe07505
Sandeep Joshi <ysandeepjoshi@gmail.com>
2024-04-28 17:46:41 -0400

    using switch case to check types

--- dist/index.js
+++ dist/index.js
@@ -12,9 +12,11 @@ function calculateTax(amount, format) {
     return format ? `${calcAmount.toFixed(2)}` : calcAmount;
 }
 let taxValue = calculateTax(100, false);
-if (typeof taxValue === "number") {
-    console.log(`Number Value: ${taxValue.toFixed(2)}`);
-}
-else if (typeof taxValue === "string") {
-    console.log(`String Value: ${taxValue.charAt(0)}`);
+switch (typeof taxValue) {
+    case "number":
+        console.log(`Number Value: ${taxValue.toFixed(2)}`);
+        break;
+    case "string":
+        console.log(`String Value: ${taxValue.charAt(0)}`);
+        break;
 }
--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,12 @@ function calculateTax(amount:number,format:boolean):string |  number {
 
 let taxValue = calculateTax(100,false);
 
-if(typeof taxValue === "number"){
+switch (typeof taxValue) {
+    case "number":
     console.log(`Number Value: ${taxValue.toFixed(2)}`);
+        break;
+        case "string":
+            console.log(`String Value: ${taxValue.charAt(0)}`);
+            break;
 }
-else if(typeof taxValue === "string"){
-    console.log(`String Value: ${taxValue.charAt(0)}`);
-}
+
