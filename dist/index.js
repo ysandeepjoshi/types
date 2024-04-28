5f17639bfe738c1d190f31db73b64e989e7a1559
Sandeep Joshi <ysandeepjoshi@gmail.com>
2024-04-28 17:14:01 -0400

    Creating a static type with a type annotation

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,8 @@ console.log(`${myVar} = ${typeof myVar}`);
 function calculateTax(amount) {
     return amount * 1.2;
 }
-console.log(`${12} = ${calculateTax(12)}`);
-console.log(`${"Hello"} = ${calculateTax("Hello")}`);
-console.log(`${true} = ${calculateTax(true)}`);
+let price = 100;
+let taxAmount = calculateTax(price);
+let halfShare = taxAmount / 2;
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half amount in tax: ${halfShare}`);
--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,11 @@ console.log(`${myVar} = ${typeof myVar}`);
 myVar = true;
 console.log(`${myVar} = ${typeof myVar}`);
 
-function calculateTax(amount) {
+function calculateTax(amount:number):number {
     return amount * 1.2;
 }
-console.log(`${12} = ${calculateTax(12)}`);
-console.log(`${"Hello"} = ${calculateTax("Hello")}`);
-console.log(`${true} = ${calculateTax(true)}`);
+let price: number = 100;
+let taxAmount:number = calculateTax(price);
+let halfShare:number = taxAmount/2;
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half amount in tax: ${halfShare}`);
