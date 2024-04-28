6e8a37d750359d1b6ae926e0be88fafe5038b389
Sandeep Joshi <ysandeepjoshi@gmail.com>
2024-04-28 17:10:04 -0400

    Displaying the variable type

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
 console.log("Hello, TypeScript");
+let myVar;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = 12;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = "Hello";
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = true;
+console.log(`${myVar} = ${typeof myVar}`);
+function calculateTax(amount) {
+    return amount * 1.2;
+}
+console.log(`${12} = ${calculateTax(12)}`);
+console.log(`${"Hello"} = ${calculateTax("Hello")}`);
+console.log(`${true} = ${calculateTax(true)}`);
